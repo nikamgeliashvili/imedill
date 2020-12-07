0c262cd7c8f611cb8b728217248b745f2501464a
nikamgeliashvili <mgeliashvilia@gmail.com>
2020-12-07 04:46:06 +0400

    pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+$(window).on("load",function(){
+    $(".loader").fadeOut("slow");
+});
+
+
+
+
+
+
+        $(".see-more").click(function(){
+            $(this).prev().toggleClass('textbox', 2000 );
+          });
+
+
+
+
+  
+          
